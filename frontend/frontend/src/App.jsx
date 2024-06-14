52a28b4d260d599cb41e506516b6887e6c30ac33
Cemile Ataman <cemileataman444@gmail.com>
2024-06-14 16:58:49 +0300

    shop page components

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import HomePage from "./pages/HomePage";
 import "./App.css";
 import React from "react";
+import ShopPage from "./pages/ShopPage";
 
 function App() {
   return (
     <React.Fragment>
-      <HomePage/>
+      {/* <HomePage/> */}
+      <ShopPage/>
       
     </React.Fragment>
   );
--- frontend/frontend/src/pages/ShopPage.jsx
+++ frontend/frontend/src/pages/ShopPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Fragment } from "react";
+import Header from "../components/Layout/Header/Header";
+import Categories from "../components/Categories/Categories";
+import Products from "../components/Products/Products";
+import CampaignSingle from "../components/CampaignSingle/CampaignSingle";
+import Policy from "../components/Layout/Policy/Policy";
+import Footer from "../components/Layout/Footer/Footer";
 
 function ShopPage() {
   return (
-    <div>ShopPage</div>
-  )
+    <Fragment>
+      <Header />
+      <Categories />
+      <Products />
+      <CampaignSingle />
+      <Products />
+      <Policy />
+      <Footer/>
+    </Fragment>
+  );
 }
 
-export default ShopPage
+export default ShopPage;
