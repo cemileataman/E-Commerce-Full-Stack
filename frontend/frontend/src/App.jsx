4aca0df4f2b385f71ab66dcb1d281161fbe12e0a
Cemile Ataman <cemileataman444@gmail.com>
2024-06-14 10:53:09 +0300

    first commit

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Policy from "./components/Policy";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Policy/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
