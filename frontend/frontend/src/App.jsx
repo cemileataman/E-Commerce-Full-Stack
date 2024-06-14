090aee24341726d253f5e81d73cd1eb5dec1bc28
Cemile Ataman <cemileataman444@gmail.com>
2024-06-14 14:08:02 +0300

    products component

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import "./App.css";
 import Categories from "./components/Categories/Categories";
 import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
 import Policy from "./components/Layout/Policy/Policy";
+import Products from "./components/Products/Products";
 import Slider from "./components/Slider/Sliders";
+import "./App.css";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Slider/>
       <Categories/>
+      <Products/>
       <Policy />
       <Footer />
     </div>
