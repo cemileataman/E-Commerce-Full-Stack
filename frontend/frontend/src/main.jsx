3250b389c2e5576ca073880fd264462c633b8290
Cemile Ataman <cemileataman444@gmail.com>
2024-06-27 13:08:30 +0300

    gallery active image

--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
@@ -8,9 +7,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <MainLayout>
       <App />
     </MainLayout>
-  </React.StrictMode>
 );
