f7948293ecce49dfa27f32658b4be6b5db9a6c1b
Cemile Ataman <cemileataman444@gmail.com>
2024-06-28 10:23:24 +0300

    basket localStoreage operations

--- frontend/frontend/src/context/CartProvider.jsx
+++ frontend/frontend/src/context/CartProvider.jsx
@@ -1,11 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [cartItems, setCartItems] = useState([]);
-  console.log("cart-items", cartItems);
+  const [cartItems, setCartItems] = useState(
+    localStorage.getItem("cartItems")
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : []
+  );
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
   const addToCart = (cartItem) => {
     // setCartItems([...cartItems, cartItem]); 1. yol
     setCartItems((prevCart) => [...prevCart, cartItem]);
