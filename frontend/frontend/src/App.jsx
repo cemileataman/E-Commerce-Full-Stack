781d10bcad9bb30274a13e6f74b14293b3a5f221
Cemile Ataman <cemileataman444@gmail.com>
2024-06-28 11:52:47 +0300

    page router dom setup

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./App.css";
-// import HomePage from "./pages/HomePage";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 // import ShopPage from "./pages/ShopPage";
 // import ContactPage from "./pages/ContactPage";
 import CartPage from "./pages/CartPage";
@@ -8,19 +8,13 @@ import CartPage from "./pages/CartPage";
 // import BlogPage from "./pages/BlogPage";
 // import BlogDetailsPage from "./pages/BlogDetailsPage";
 // import ProductDetailsPage from "./pages/ProductDetailsPage";
+import "./App.css";
 
 function App() {
   return (
-    <React.Fragment>
-      {/* <HomePage/> */}
-      {/* <ShopPage/> */}
-      {/* <ContactPage/> */}
-      {/* <AuthPage/> */}
-      <CartPage/>
-      {/* <BlogPage/> */}
-     {/* <BlogDetailsPage/> */}
-     {/* <ProductDetailsPage/> */}
-    </React.Fragment>
+    <Routes>
+     <Route path="/" element={<HomePage/>}/>
+    </Routes>
   );
 }
 
--- frontend/frontend/src/main.jsx
+++ frontend/frontend/src/main.jsx
@@ -1,15 +1,18 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.jsx";
-import "./index.css";
+import App from "./App.jsx";
+import CartProvider from "./context/CartProvider.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import CartProvider from "./context/CartProvider.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <CartProvider>
-    <MainLayout>
-      <App />
-    </MainLayout>
-  </CartProvider>
+  <BrowserRouter>
+    <CartProvider>
+      <MainLayout>
+        <App />
+      </MainLayout>
+    </CartProvider>
+  </BrowserRouter>
 );
