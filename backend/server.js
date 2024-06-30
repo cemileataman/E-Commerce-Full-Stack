48b66dde0f3ae5832d4ded31760460c48d205c40
Cemile Ataman <cemileataman444@gmail.com>
2024-06-30 18:00:18 +0300

    mongodb setup

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const port = 5000;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://cemile-e-commerce:vnUCkuqpwFakyB6n@mern-e-commerce.srbcgso.mongodb.net/?retryWrites=true&w=majority&appName=mern-e-commerce"
+    );
+    console.log("Connected to MongoDb");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Hello Express.js");
 });
 
-app.get ("/api",(req,res)=>{
-    res.send("This is root");
+app.get("/api", (req, res) => {
+  res.send("This is root");
 });
 
-app.listen(5000,()=>{
-    console.log(`Sunucu ${5000} portunda çalışıyor`);
+app.listen(port, () => {
+  connect();
+  console.log(`Sunucu ${port} portunda çalışıyor`);
 });
