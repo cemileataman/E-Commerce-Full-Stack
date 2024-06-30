0b100b52a51aa3ff5c1b5b310ecdac0d2860a9b7
Cemile Ataman <cemileataman444@gmail.com>
2024-06-30 13:07:06 +0300

    page redirection

--- frontend/frontend/src/components/Layout/Header/Header.jsx
+++ frontend/frontend/src/components/Layout/Header/Header.jsx
@@ -1,10 +1,13 @@
 import { useContext } from "react";
-import "./Header.css";
 import PropTypes from "prop-types";
+import { Link, useLocation } from "react-router-dom";
 import { CartContext } from "../../../context/CartProvider";
+import "./Header.css";
 
 function Header({ setIsSearchShow }) {
-  const {cartItems} =useContext(CartContext);
+  const { cartItems } = useContext(CartContext);
+
+  const { pathname } = useLocation();
   return (
     <header>
       <div className="global-notification">
@@ -22,18 +25,21 @@ function Header({ setIsSearchShow }) {
               <i className="bi bi-list" id="btn-menu"></i>
             </div>
             <div className="header-left">
-              <a href="index.html" className="logo">
+            <Link to={"/"} className="logo">
                 LOGO
-              </a>
+                </Link>
             </div>
             <div className="header-center" id="sidebar">
               <nav className="navigation">
                 <ul className="menu-list">
                   <li className="menu-list-item">
-                    <a href="index.html" className="menu-link active">
+                  <Link
+                      to={"/"}
+                      className={`menu-link ${pathname === "/" && "active"}`}
+                    >
                       HOME
                       <i className="bi bi-chevron-down"></i>
-                    </a>
+                      </Link>
                     <div className="menu-dropdown-wrapper">
                       <ul className="menu-dropdown-content">
                         <li>
@@ -68,10 +74,15 @@ function Header({ setIsSearchShow }) {
                     </div>
                   </li>
                   <li className="menu-list-item megamenu-wrapper">
-                    <a href="shop.html" className="menu-link">
+                  <Link
+                      to={"/shop"}
+                      className={`menu-link ${
+                        pathname === "/shop" && "active"
+                      }`}
+                    >
                       SHOP
                       <i className="bi bi-chevron-down"></i>
-                    </a>
+                      </Link>
                     <div className="menu-dropdown-wrapper">
                       <div className="menu-dropdown-megamenu">
                         <div className="megamenu-links">
@@ -172,16 +183,26 @@ function Header({ setIsSearchShow }) {
                     </div>
                   </li>
                   <li className="menu-list-item">
-                    <a href="blog.html" className="menu-link">
-                      {" "}
-                      BLOG{" "}
-                    </a>
+                  <Link
+                      to={"/blog"}
+                      className={`menu-link ${
+                        pathname === "/blog" && "active"
+                      }`}
+                    >
+                      
+                      BLOG
+                      </Link>
                   </li>
                   <li className="menu-list-item">
-                    <a href="contact.html" className="menu-link">
+                  <Link
+                      to={"/contact"}
+                      className={`menu-link ${
+                        pathname === "/contact" && "active"
+                      }`}
+                    >
                       {" "}
                       CONTACT{" "}
-                    </a>
+                      </Link>
                   </li>
                 </ul>
               </nav>
@@ -189,9 +210,9 @@ function Header({ setIsSearchShow }) {
             </div>
             <div className="header-right">
               <div className="header-right-links">
-                <a href="account.html" className="header-account">
+               <Link to={"/auth"} className="header-account">
                   <i className="bi bi-person"></i>
-                </a>
+                  </Link>
                 <button
                   className="search-button"
                   onClick={() => setIsSearchShow(true)}
@@ -202,10 +223,12 @@ function Header({ setIsSearchShow }) {
                   <i className="bi bi-heart"></i>
                 </a>
                 <div className="header-cart">
-                  <a href="cart.html" className="header-cart-link">
+                <Link to={"/cart"} className="header-cart-link">
                     <i className="bi bi-bag"></i>
-                    <span className="header-cart-count">{cartItems.length}</span>
-                  </a>
+                    <span className="header-cart-count">
+                      {cartItems.length}
+                    </span>
+                    </Link>
                 </div>
               </div>
             </div>
--- frontend/frontend/src/components/Products/ProductItem.jsx
+++ frontend/frontend/src/components/Products/ProductItem.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import "./ProductItem.css";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { CartContext } from "../../context/CartProvider";
 
 const ProductItem = ({ productItem }) => {
   const { cartItems, addToCart } = useContext(CartContext);
-
   const filteredCart = cartItems.find(
     (cartItem) => cartItem.id === productItem.id
   );
@@ -60,9 +60,9 @@ const ProductItem = ({ productItem }) => {
           <button>
             <i className="bi bi-heart-fill"></i>
           </button>
-          <a href="#" className="product-link">
+          <Link to={`product/${productItem.id}`} className="product-link">
             <i className="bi bi-eye-fill"></i>
-          </a>
+          </Link>
           <a href="#">
             <i className="bi bi-share-fill"></i>
           </a>
