9fedfa465e964e10871ca42aac1667c606b16e9f
Cemile Ataman <cemileataman444@gmail.com>
2024-06-30 14:01:08 +0300

    express.js setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express.js");
+});
+
+app.get ("/api",(req,res)=>{
+    res.send("This is root");
+});
+
+app.listen(5000,()=>{
+    console.log(`Sunucu ${5000} portunda çalışıyor`);
+});
