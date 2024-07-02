9678df8d765e501b1f65b5b9d6eb879835036cd3
Cemile Ataman <cemileataman444@gmail.com>
2024-07-02 13:06:33 +0300

    create new category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category.js");
 
-//!Tüm kategorileri getirme(Read-All)
+router.post("/", async (req, res) => {
+  try {
+    const { name, img } = req.body;
+
+    const newCategory = new Category({ name, img });
+    await newCategory.save();
+
+    res.status(201).json(newCategory);
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Tüm kategorileri getirme (Read- All)
 router.get("/", async (req, res) => {
-  res.send("kategoriler getirildi");
+  res.send("Kategoriler getirildi!");
 });
 
 module.exports = router;
