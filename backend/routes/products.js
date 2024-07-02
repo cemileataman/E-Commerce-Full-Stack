77d9d634051d0b85f7a331adc178e454042c7469
Cemile Ataman <cemileataman444@gmail.com>
2024-07-03 01:04:18 +0300

    product retrieval

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
 const Product = require("../models/Product.js");
 
 // Yeni bir ürün oluşturma (Create)
@@ -13,4 +14,39 @@ router.post("/", async (req, res) => {
     console.log(error);
   }
 });
+
+// Tüm ürünleri getirme (Read - All)
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+// Belirli bir ürünü getirme (Read - Single)
+router.get("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    if (!mongoose.Types.ObjectId.isValid(productId)) {
+      return res.status(400).json({ error: "Invalid product ID." });
+    }
+
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
 module.exports = router;
