d21583162ddd0b7b4b8e41245b687468d0cb67e0
Cemile Ataman <cemileataman444@gmail.com>
2024-07-02 23:59:35 +0300

    create new user

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
+
 //diğer rota dosyalarını içe aktarıyoruz.
 const productRoute = require("./products.js");
 const categoryRoute = require("./categories.js");
+const authRoute = require("./auth.js");
 
 //Her rotayı ilgili yol altında kullanıyoruz.
 router.use("/categories", categoryRoute);
+router.use("/auth", authRoute);
 router.use("/products", productRoute);
 
 module.exports = router;
