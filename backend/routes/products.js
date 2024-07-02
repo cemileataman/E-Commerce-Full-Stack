3e1f01f7a347299bccbd24b4cadfa1ffdd3df46c
Cemile Ataman <cemileataman444@gmail.com>
2024-07-03 00:47:07 +0300

    create new product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.js");
 
-//!Tüm ürünleri getirme(Read-All)
-router.get("/", async (req, res) => {
-  res.send("Ürünler getirildi");
+// Yeni bir ürün oluşturma (Create)
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log(error);
+  }
 });
-
 module.exports = router;
