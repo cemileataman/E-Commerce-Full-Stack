fd6606c775fa78bea2249aaf6f5bae5c4250e208
Cemile Ataman <cemileataman444@gmail.com>
2024-07-02 12:06:31 +0300

    update

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: 'localhost',
     open: true,
+    https: false, // HTTPS'i devre dışı bırakmak için
   }
 })
