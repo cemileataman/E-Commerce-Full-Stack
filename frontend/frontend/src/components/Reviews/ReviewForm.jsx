6d586a83de6efaeadff3e4c8fd2eeecaec6def27
Cemile Ataman <cemileataman444@gmail.com>
2024-07-04 21:09:11 +0300

    review form component settings

--- frontend/frontend/src/components/Reviews/ReviewForm.jsx
+++ frontend/frontend/src/components/Reviews/ReviewForm.jsx
@@ -1,79 +1,107 @@
+import { useState } from "react";
+
 const ReviewForm = () => {
+  const [rating, setRating] = useState(0);
+  const [review, setReview] = useState("");
+
+  const handleRatingChange = (e, newRating) => {
+    e.preventDefault();
+    setRating(newRating);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = {
+      rating: rating,
+      review: review,
+    };
+
+    console.log(formData);
+  };
+
   return (
-    <div className="review-form-wrapper">
-      <h2>Add a review</h2>
-      <form className="comment-form">
-        <p className="comment-notes">
-          Your email address will not be published. Required fields are marked
+    <form className="comment-form" onSubmit={handleSubmit}>
+      <p className="comment-notes">
+        Your email address will not be published. Required fields are marked
+        <span className="required">*</span>
+      </p>
+      <div className="comment-form-rating">
+        <label>
+          Your rating
           <span className="required">*</span>
-        </p>
-        <div className="comment-form-rating">
-          <label>
-            Your rating
-            <span className="required">*</span>
-          </label>
-          <div className="stars">
-            <a href="#" className="star">
-              <i className="bi bi-star-fill"></i>
-            </a>
-            <a href="#" className="star">
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-            </a>
-            <a href="#" className="star">
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-            </a>
-            <a href="#" className="star">
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-            </a>
-            <a href="#" className="star">
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-              <i className="bi bi-star-fill"></i>
-            </a>
-          </div>
+        </label>
+        <div className="stars">
+          <a
+            href="#"
+            className={`star ${rating === 1 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 1)}
+          >
+            <i className="bi bi-star-fill"></i>
+          </a>
+          <a
+            href="#"
+            className={`star ${rating === 2 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 2)}
+          >
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+          </a>
+          <a
+            href="#"
+            className={`star ${rating === 3 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 3)}
+          >
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+          </a>
+          <a
+            href="#"
+            className={`star ${rating === 4 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 4)}
+          >
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+          </a>
+          <a
+            href="#"
+            className={`star ${rating === 5 && "active"}`}
+            onClick={(e) => handleRatingChange(e, 5)}
+          >
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+            <i className="bi bi-star-fill"></i>
+          </a>
         </div>
-        <div className="comment-form-comment form-comment">
-          <label htmlFor="comment">
-            Your review
-            <span className="required">*</span>
-          </label>
-          <textarea id="comment" cols="50" rows="10"></textarea>
-        </div>
-        <div className="comment-form-author form-comment">
-          <label htmlFor="name">
-            Name
-            <span className="required">*</span>
-          </label>
-          <input id="name" type="text" />
-        </div>
-        <div className="comment-form-email form-comment">
-          <label htmlFor="email">
-            Email
-            <span className="required">*</span>
-          </label>
-          <input id="email" type="email" />
-        </div>
-        <div className="comment-form-cookies">
-          <input id="cookies" type="checkbox" />
-          <label htmlFor="cookies">
-            Save my name, email, and website in this browser for the next time I
-            comment.
-            <span className="required">*</span>
-          </label>
-        </div>
-        <div className="form-submit">
-          <input type="submit" className="btn submit" />
-        </div>
-      </form>
-    </div>
+      </div>
+      <div className="comment-form-comment form-comment">
+        <label htmlFor="comment">
+          Your review
+          <span className="required">*</span>
+        </label>
+        <textarea
+          id="comment"
+          cols="50"
+          rows="10"
+          onChange={(e) => setReview(e.target.value)}
+        ></textarea>
+      </div>
+      <div className="comment-form-cookies">
+        <input id="cookies" type="checkbox" />
+        <label htmlFor="cookies">
+          Save my name, email, and website in this browser for the next time I
+          comment.
+          <span className="required">*</span>
+        </label>
+      </div>
+      <div className="form-submit">
+        <input type="submit" className="btn submit" />
+      </div>
+    </form>
   );
 };
 
