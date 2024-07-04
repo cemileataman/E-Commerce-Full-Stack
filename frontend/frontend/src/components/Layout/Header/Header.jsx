51332dd1d0320b00c5bff086247890961a76543d
Cemile Ataman <cemileataman444@gmail.com>
2024-07-04 08:46:07 +0300

    login process

--- frontend/frontend/src/components/Layout/Header/Header.jsx
+++ frontend/frontend/src/components/Layout/Header/Header.jsx
@@ -7,6 +7,7 @@ import "./Header.css";
 function Header({ setIsSearchShow }) {
   const { cartItems } = useContext(CartContext);
 
+  const user = localStorage.getItem("user");
   const { pathname } = useLocation();
   return (
     <header>
@@ -219,9 +220,9 @@ function Header({ setIsSearchShow }) {
                 >
                   <i className="bi bi-search"></i>
                 </button>
-                <a href="#">
+                {/* <a href="#">
                   <i className="bi bi-heart"></i>
-                </a>
+                </a> */}
                 <div className="header-cart">
                 <Link to={"/cart"} className="header-cart-link">
                     <i className="bi bi-bag"></i>
@@ -230,6 +231,25 @@ function Header({ setIsSearchShow }) {
                     </span>
                     </Link>
                 </div>
+                {user && (
+                  <button
+                    className="search-button"
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          "Çıkış yapmak istediğinize emin misiniz?"
+                        )
+                      ) {
+                        {
+                          localStorage.removeItem("user");
+                          window.location.href = "/";
+                        }
+                      }
+                    }}
+                  >
+                    <i className="bi bi-box-arrow-right"></i>
+                  </button>
+                )}
               </div>
             </div>
           </div>
